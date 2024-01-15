689dfa67062a3a8ee1ab2d9902011dba836a973c
imchopi <apchopito@gmail.com>
2024-01-15 10:31:09 +0100

    Init

---
+++ src/test/java/com/example/Incident/IncidentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Incident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
